1c0a23a337cbb0d09703d4fe60066951b9546f0b
beelenvp <patrick.van.beelen@rivm.nl>
2022-06-30 16:50:05 +0200

    test loopt nog niet goed nieuwe functie wordt niet gezien

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,3 +1,18 @@
+#' Split a string with stringr
+#'
+#' @param x A single character vector
+#' @param split Character to remove with split
+#'
+#' @return Vector of strings
+#' @export
+#'
+#' @examples
+#'  x <- "alfa,bravo,charlie,delta"
+#'  output<-str_split_one(x,pattern=",")
+#'  output[3]=="charlie"
+#'  y<-"alfa,bravo,charlie,delta"
+#'  str_split_one(y)
+#'  str_split_one(y,pattern="a")
 str_split_one <- function(string, pattern=",", n = Inf) {
   stopifnot(is.character(string), length(string) <= 1)
   if (length(string) == 1) {
--- tests/testthat/test-str_split_one.R
+++ tests/testthat/test-str_split_one.R
@@ -1,3 +1,3 @@
-test_that("multiplication works", {
+test_that("multipliction works", {
   expect_equal(str_split_one("qwert,yuio,pasd,fghjkl")[2], "yuio")
 })
